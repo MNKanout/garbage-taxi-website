84575e0b7e7eebca08aa3ae3884c59fccf8924dc
Nour <mnkanout@gmail.com>
2021-12-16 02:56:45 +0100

    added navbutton

--- static/script.js
+++ static/script.js
@@ -3,3 +3,7 @@ function scrollToSection(element) {
     var element = document.getElementById(element);
     element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
 }
+
+function NavToggle(x) {
+    x.classList.toggle("change");
+  }
